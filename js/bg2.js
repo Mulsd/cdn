2dcd562628ae1cb3fa0d88c6c4de36ade1207eac
Mulsd <Mulsd@qq.com>
2020-07-05 12:45:23 +0800

    U

--- js/bg2.js
+++ js/bg2.js
@@ -7,6 +7,7 @@ xiaokang.imgList = [
     "https://cdn.jsdelivr.net/gh/Mulsd/img-bed@latest/2020/07/05/0db95fb7e3bccb9ff012253a103e46fa.png",
     "https://cdn.jsdelivr.net/gh/Mulsd/img-bed@latest/2020/07/05/5eca656b76701a8bb7a6f237eaa54d00.png",
     "https://cdn.jsdelivr.net/gh/Mulsd/img-bed@latest/2020/07/05/220ca21ab67977f80763cfa0b739e7e8.png",
+    //111111
 ];
 // 调用随机背景
 xiaokang.randomBg();
